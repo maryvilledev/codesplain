623bdf8fb9c6971fea6e62411f88ba75699e445b
Brian Hoehne <brian.hoehne@maryville.com>
2017-03-23 13:14:57 -0500

    don't use sync

--- publish.sh
+++ publish.sh
@@ -13,11 +13,11 @@ VERSION_TAG="$2"
 
 if [ "$PUSH_ENV" == "release" ]
 then
-    S3CMD="put"
+  echo "Releasing Codesplain Parsers"
 elif [ "$PUSH_ENV" == "dev" ]
 then
-    S3CMD="sync"
-    VERSION_TAG="dev"
+  echo "Updating Codesplain Parsers in dev environment"
+  VERSION_TAG="dev"
 else
    echo "Error! Invalid PUSH_ENV" 1>&2
    exit 1
@@ -39,8 +39,8 @@ rm -rf "$CACHE_DIR/$LANG/"
 ./node_modules/.bin/webpack "--env.langs=$LANG" --env.optimize=0 --env.enable_debug=1
 ./node_modules/.bin/webpack "--env.langs=$LANG" --env.optimize=1 --env.enable_debug=0
 
-echo "S3Command $S3CMD"
-aws s3 $S3CMD $OUTPUT_DIR/$LANG.min.js s3://codesplain-parsers/$LANG/$VERSION_TAG/$LANG.min.js
-aws s3 $S3CMD $OUTPUT_DIR/$LANG.js s3://codesplain-parsers/$LANG/$VERSION_TAG/$LANG.js
+echo "S3Command mv $OUTPUT_DIR/$LANG.min.js s3://codesplain-parsers/$LANG/$VERSION_TAG/$LANG.min.js"
+aws s3 mv $OUTPUT_DIR/$LANG.min.js s3://codesplain-parsers/$LANG/$VERSION_TAG/$LANG.min.js
+aws s3 mv $OUTPUT_DIR/$LANG.js s3://codesplain-parsers/$LANG/$VERSION_TAG/$LANG.js
 
 # done
