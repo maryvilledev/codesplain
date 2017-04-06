81a1affc7bfc29a7e30720fbd47a287e745b577c
Dane Johnson <dane_johnson@ymail.com>
2017-04-06 15:47:20 -0500

    added unit test to look for all mappings in csv

---
+++ __tests__/csv.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+describe('mappings', () => {
+  fs.readdirSync('./language_configs/').filter((name) =>
+    name.split('.')[1] === 'rules'
+  ).forEach((file) => {
+    describe(file.split('.')[0] + '.csv', () => {
+      it('should have a definition for all rules', () => {
+        const csv = fs.readFileSync(`./mappings/${file.split('.')[0]}.csv`,
+      {encoding: 'utf8'})
+          .split('\n')
+          .slice(1) //Cut off the header
+          .map((line) => line.split(',')[0])
+          .sort((a, b) => a.localeCompare(b))
+          .slice(1); //Cut off the newline
+        Object.keys(require(`../language_configs/${file}`))
+          .sort((a, b) => a.localeCompare(b))
+          .forEach((rule, i) => expect(rule).toEqual(csv[i]));
+      });
+    });
+  });
+});
