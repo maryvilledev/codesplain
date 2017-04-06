8379c28a7e02b3aa8b23e84bc7d4858e3d2bce95
Dane Johnson <dane_johnson@ymail.com>
2017-04-06 16:46:57 -0500

    Added test to ensure all enabled tokens have pretty names and colors

--- __tests__/csv.js
+++ __tests__/csv.js
@@ -5,18 +5,24 @@ describe('mappings', () => {
     name.split('.')[1] === 'rules'
   ).forEach((file) => {
     describe(file.split('.')[0] + '.csv', () => {
+      const csvFile = `./mappings/${file.split('.')[0]}.csv`;
+      const csv = fs.readFileSync(csvFile, {encoding: 'utf8'})
+        .split('\n')
+        .slice(1, -1); //Cut off the header and newline
       it('should have a definition for all rules', () => {
-        const csvFile = `./mappings/${file.split('.')[0]}.csv`;
-        const csv = fs.readFileSync(csvFile, {encoding: 'utf8'})
-          .split('\n')
-          .slice(1) //Cut off the header
-          .map((line) => line.split(',')[0])
-          .sort((a, b) => a.localeCompare(b))
-          .slice(1); //Cut off the newline
+        const token = csv.map((line) => line.split(',')[0])
+          .sort((a, b) => a.localeCompare(b));
         Object.keys(require(`../language_configs/${file}`))
           .sort((a, b) => a.localeCompare(b))
-          .forEach((rule, i) => expect(rule).toEqual(csv[i]));
+          .forEach((rule, i) => expect(rule).toEqual(token[i]));
       });
+      it('should have a pretty name and color for enabled rules', () => {
+        csv.filter((line) => line.split(',')[1] != 0).forEach((line) => {
+          const cols = line.split(',');
+          expect(cols[2]).toBeTruthy();
+          expect(cols[3]).toMatch(/#[a-fA-F0-9]{6}/);
+        })
+      })
     });
   });
 });
