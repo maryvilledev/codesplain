113466585e533c558bb11f1296113b16c1b00515
Hopding <andrew.dillon.j@gmail.com>
2017-04-12 21:17:11 -0500

    Did some significant refactoring to compile.js; WIP

---
+++ __tests__/transformers/simplify_node.test.js
@@ -0,0 +1,7 @@
+describe(`simplify_node.js`, () => {
+  const simplify_node = require('../../app/transformers/simplify_node.js');
+
+  it(`is a function`, () => {
+    expect(typeof(simplify_node)).toEqual('function');
+  });
+});
