219498205c4fb78719c0c37a15f8f40311ce4772
Joel Walker <joel.walker@maryville.com>
2017-05-18 14:52:56 -0500

    Removed JavaScript antlr runtime dependency

--- language_configs/java8.compile.js
+++ language_configs/java8.compile.js
@@ -3,7 +3,6 @@
 module.exports = {
 	'grammar_files': {
         'Java': 'Java8.g4',
-        'JavaScript': 'Java8.JavaScriptTarget.g4',
         'TypeScript': 'Java8.TypeScriptTarget.g4',
     },
     'needs_java_func_data': true,
--- language_configs/python3.compile.js
+++ language_configs/python3.compile.js
@@ -3,7 +3,6 @@
 module.exports = {
 	'grammar_files': {
         'Java': 'Python3.g4',
-        'JavaScript': 'Python3.JavaScriptTarget.g4',
         'TypeScript': 'Python3.TypeScriptTarget.g4',
     },
     'tree_matcher_specs': require('./python3.tree_matcher_specs.js'),
